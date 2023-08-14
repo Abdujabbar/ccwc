66f25f54144153064d6cf59252c90fde8500563a
Abdujabbar MIRKHALIKOV <abdujabbar.mirkhalikov@prestataire.sewan.fr>
2023-08-14 12:03:57 +0500

    Little modifications for make code more readable.

--- ccwc/command.py
+++ ccwc/command.py
@@ -6,24 +6,24 @@ from ccwc.cli import get_user_input
 def run(args):
     lines = []
     if not args.files:
-        raw = process(TextIOWrapper(BytesIO(get_user_input().encode())), args)
-        lines.append(to_str(raw))
+        with BytesIO(get_user_input().encode()) as stream:
+            raw = process(TextIOWrapper(stream), args)
+            lines.append(to_str(raw))
     else:
         for file in args.files:
             try:
                 with open(file) as stream:
                     raw = process(stream, args)
                     raw.append(f"{file}")
-
                     lines.append(to_str(raw))
-            except Exception as ex:
-                lines.append(str(ex))
+            except PermissionError:
+                lines.append(f"Not enough permission for read: {file}")
+            except FileNotFoundError:
+                lines.append(f"File {file} doesn't exists")
+            
 
     return "\n".join(lines)
 
 
 def to_str(arr):
-    for x in range(len(arr)):
-        arr[x] = f"{arr[x]:7} "
-
-    return " ".join(arr)
+    return " ".join([f"{arr[x]:7}" for x in range(len(arr))])
